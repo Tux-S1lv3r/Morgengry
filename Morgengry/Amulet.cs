299ac57e5d0cbcb54ed88b41f4028503ac75aa9e
tux-S1lv3r <faiz0095@edu.eal.dk>
2017-10-23 10:40:57 +0200

    Add project files.

---
+++ Morgengry/Amulet.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morgengry
+{
+    public enum Level
+    {
+        low, medium, high
+    };
+    public class Amulet
+    {
+        public string ItemId { get; set; }
+        public string Design { get; set; }
+        public Level Quality { get; set; }
+        public Amulet(string itemId, Level quality, string design)
+        {
+            ItemId = itemId;
+            Quality = quality;
+            Design = design;
+        }
+        public Amulet(string itemId):
+            this (itemId, Level.medium, "")
+        { 
+
+        }
+        public Amulet(string itemId, Level quality):
+            this(itemId, quality, "")
+        {
+
+        }
+        public override string ToString()
+        {
+            string convertAmuletPropertiesToString;
+            convertAmuletPropertiesToString = "ItemId: " + ItemId + ", Quality: " + Quality + ", Design: " + Design.ToString();
+            return convertAmuletPropertiesToString;
+        }
+    }
+}
